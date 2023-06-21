5b48655b1e12b38904f0021b165ced3c140874ef
Laura Hope <ssteamlaura@gmail.com>
2023-06-21 13:33:30 -0700

    Add more plant functionality/properties

--- src/index.js
+++ src/index.js
@@ -1,35 +1,40 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Triangle from './js/triangle.js';
-import Rectangle from './js/rectangle.js';
+import { newPlant, storeState, stateControl, changeState, superFood, superWater, superLight } from './js/plant.js';
+ 
 
-function handleTriangleForm() {
-  event.preventDefault();
-  document.querySelector('#response').innerText = null;
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const triangle = new Triangle(length1, length2, length3);
-  const response = triangle.checkType();
-  const pTag = document.createElement("p");
-  pTag.append(`Your result is: ${response}.`);
-  document.querySelector('#response').append(pTag);
-}
+if (typeof window !== 'undefined') {
+  window.onload = function() {
 
-function handleRectangleForm() {
-  event.preventDefault();
-  document.querySelector('#response2').innerText = null;
-  const length1 = parseInt(document.querySelector('#rect-length1').value);
-  const length2 = parseInt(document.querySelector('#rect-length2').value);
-  const rectangle = new Rectangle(length1, length2);
-  const response = rectangle.getArea();
-  const pTag = document.createElement("p");
-  pTag.append(`The area of the rectangle is ${response}.`);
-  document.querySelector('#response2').append(pTag);
-}
+  document.getElementById('feed').onclick = function() {
+    const newState = stateControl(superFood);
+    document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+  };
 
-window.addEventListener("load", function() {
-  document.querySelector("#triangle-checker-form").addEventListener("submit", handleTriangleForm);
-  document.querySelector("#rectangle-area-form").addEventListener("submit", handleRectangleForm);
-});
+  document.getElementById('water').onclick = function() {
+    const newState = stateControl(superWater);
+    document.getElementById('water-value').innerText = `Water: ${newState.water}`;
+  };
+
+  document.getElementById('light').onclick = function() {
+    const newState = stateControl(superLight);
+    document.getElementById('light-value').innerText = `Light: ${newState.light}`;
+  };
+};
+};
+
+  // document.getElementById('show-state').onclick = function() {
+  //   const currentState = stateControl();
+  //   document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
+  // };
+
+  // document.getElementById('new-plant').onclick = function() {
+  //   const newPlant = storeState();
+  //   document.getElementById('soil-value').innerText = `Soil: ${newPlant.soil}`;
+  //   document.getElementById('water-value').innerText = `Water: ${newPlant.water}`;
+  //   document.getElementById('light-value').innerText = `Light: ${newPlant.light}`;
+  // };
+
+storeState();
+changeState();
--- src/js/plant.js
+++ src/js/plant.js
@@ -1,6 +1,85 @@
-const hydrate = (plant) => {
-  return {
-    ...plant,
-    water: (plant.water || 0) + 1
-  }
+// This function stores our state.
+export const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  };
 };
+
+export const stateControl = storeState();
+// export const newPlant = storeState();
+
+// This is a function factory. 
+export const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    })
+  };
+};
+
+// We create four functions using our function factory. 
+export const feed = changeState("soil")(1);
+export const superFood = changeState("soil")(5);
+
+export const hydrate = changeState("water")(1);
+export const superWater = changeState("water")(5);
+
+export const giveLight = changeState("light")(1);
+export const superLight = changeState("light")(5);
+
+
+
+// CURRIED FUNCTION - is pure function that takes one argument at a time, returning a new function with the next argument, until all arguments have been passed in and the application is complete.
+// also uses closures - a function that returns another function, and the inner function can access the outer function's variables.
+// const changeState = (prop) => {
+//   return (value) => {
+//     return (state) => ({
+//       ...state,
+//       [prop]: (state[prop] || 0) + value
+//     })
+//   }
+// };
+
+// const storeState = () => {
+//   let currentState = {};
+//   return (stateChangeFunction) => {
+//     const newState = stateChangeFunction(currentState);
+//     currentState = {...newState};
+//     return newState;
+//   }
+// };
+//outer function is storeState, inner function is changeState
+//storeState does not take an argument, only job is to store the currentState of an object
+//currentState of an object will be initialized as an empty object {} - using let because we will be reassigning the currentState variable
+//outer function storeState returns an anonymous inner function that takes one parameter called stateChangeFunction - this inner function will take a function as an argument - specifies the exact change that should be made to currentState
+//the line const newState = stateChangeFunction(currentState); -- takes the function passed in as an argument and calls it with currentState as an argument - saves the result in a variable called newState
+// will then need to store our function in another constant like this:
+//  const stateControl = storeState(); // invoking the storeState() function and creating a closure over the currentState variable
+ // stateControl holds the inner function 
+
+
+
+// const hydrate = (plant) => {
+//   return {
+//     ...plant,
+//     water: (plant.water || 0) + 1
+//   }
+// };
+
+// const feed = (plant) => {
+//   return {
+//     ...plant,
+//     soil: (plant.soil || 0) + 1
+//   }
+// }; 
+
+// const changeState = (state, property) => {
+//   return {
+//     ...state,
+//     [property]: (state[property] || 0) + value
+//   }
+// };
